e0cdd45ec6db275f12c853ebacd2e021d9532400
Gustavo Carvalho <gustavo1net@hotmail.com>
2022-01-10 19:22:36 -0300

    null input

--- src/checkers.c
+++ src/checkers.c
@@ -6,11 +6,38 @@
 /*   By: glima-de <glima-de@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/30 19:47:25 by glima-de          #+#    #+#             */
-/*   Updated: 2021/12/02 19:38:19 by glima-de         ###   ########.fr       */
+/*   Updated: 2022/01/10 19:18:53 by glima-de         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../pipex.h"
+
+static int	check_null_parans(int argc, char **argv)
+{
+	int	i;
+	int	j;
+	int	pass;
+
+	i = 0;
+	while (i < argc)
+	{
+		j = 0;
+		pass = 0;
+		while (argv[i][j] != '\0')
+		{
+			if (argv[i][j] != ' ')
+			{
+				pass = 1;
+				break ;
+			}
+			j++;
+		}
+		if (!pass)
+			return (0);
+		i++;
+	}
+	return (1);
+}
 
 static int	test_and_apply_cmd(t_data *data, int index)
 {
@@ -63,6 +90,11 @@ int	check_valid_cmds(t_data *data)
 
 int	check_parans(int argc, char **argv)
 {
+	if (!check_null_parans(argc, argv))
+	{
+		ft_putstr_fd("One argument is null\n", 1);
+		return (0);
+	}
 	if (argc != 5)
 	{
 		ft_putstr_fd("Pipex works using: pipex file_in cmd cmd file_out\n", 1);
